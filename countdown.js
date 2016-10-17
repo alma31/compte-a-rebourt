562f4933b4cfd768eb95e2cd2ae6879b089c4f75
lenni <lenni31@neuf.fr>
2016-10-17 13:51:30 +0200

    vertion final

--- countdown.js
+++ countdown.js
@@ -1,42 +1,37 @@
-
-function compteur(){
-	timer--;
-	var minutes = parseInt(timer/60, 10);
-	var secondes = (timer - minutes*60);
-
-	$('#minutes').text(minutes);	
-	$('#secondes').text(secondes);	
-	if(timer==0){
-		clearInterval(compte);
-		}
-}
-
-var timer=30;
-var compte =setInterval(compteur, 1000);
-
-$('.stop').click(function(){
-	clearInterval(compte);
-});
-
-$('.play').click(function(){
-compte =setInterval(compteur, 1000);
-
-});
-
-
-$('.reset').click(function(){
-timer=30;
-//compte =setInterval(compteur, 1000);
-
-});
-
-
-
-// $(':button').click(function () {
-//     if (this.id == 'stop') {
-//         alert('Button stop was clicked');
-//     }
-//     else if (this.id == 'start') {
-//         alert('Button start was clicked');
-//     }
-// });
+(function(){
+	app = {
+		timer:10,
+		intervalID:null,
+		init: function(){
+			app.listeners();
+		},
+		listeners: function(){
+			$(".play").on('click', app.play);
+			$(".stop").on('click', app.stop);
+			$('.reset').on('click', app.reset);
+		},
+		play: function(){
+			app.intervalID = setInterval(app.decrement, 1000);
+		},
+		decrement:function(){
+			app.timer--;
+			app.updateView();
+			if(app.timer===0){
+				clearInterval(app.intervalID);
+			}
+		},
+		updateView: function(){
+			var minutes = parseInt(app.timer/60, 10);
+			var secondes = app.timer % 60;
+			$("#minutes").text(minutes);
+			$("#secondes").text(secondes);
+		},
+		reset: function(){
+			app.timer = 10;
+		},
+		stop: function(){
+			clearInterval(app.intervalID);
+		},
+	};
+	app.init();
+})();
