6b3d90a26e1c06e8e0e545eea033996037a612db
lenni <lenni31@neuf.fr>
2016-10-17 16:32:09 +0200

    indentation

--- countdown.js
+++ countdown.js
@@ -1,44 +1,41 @@
 (function(){
-	app = {
+app={
 		defaultTimer:10,
 		timer:null,
 		intervalID:null,
 		init: function(){
-			app.timer = app.defaultTimer;
-			app.listeners();
+				app.timer = app.defaultTimer;
+				app.listeners();
 		},
 		listeners: function(){
-			$(".play").on('click', app.play);
-			$(".stop").on('click', app.stop);
-			$('.reset').on('click', app.reset);
+				$(".play").on('click', app.play);
+				$(".stop").on('click', app.stop);
+				$('.reset').on('click', app.reset);
 		},
 		play: function(){
-			clearInterval(app.intervalID);
-			app.intervalID = setInterval(app.decrement, 1000);
-
+				clearInterval(app.intervalID);
+				app.intervalID = setInterval(app.decrement, 1000);
 		},
 		decrement:function(){
-			app.timer--;
-			app.updateView();
-			if(app.timer <= 0){
-				app.timer = 1;
-             
-			}
+				app.timer--;
+				app.updateView();
+				if(app.timer <= 0){
+					app.timer = 1;}
 		},
 		updateView: function(){
-			var minutes = parseInt(app.timer/60, 10);
-			var secondes = app.timer % 60;
-			$("#minutes").text(minutes);
-			$("#secondes").text(secondes);
+				var minutes = parseInt(app.timer/60, 10);
+				var secondes = app.timer % 60;
+				$("#minutes").text(minutes);
+				$("#secondes").text(secondes);
 
 		},
 		reset: function(){
-            app.timer = app.defaultTimer;
-            app.updateView();   
+				app.timer = app.defaultTimer;
+				app.updateView();   
 		},
 		stop: function(){
-			clearInterval(app.intervalID);
+				clearInterval(app.intervalID);
 		},
 	};
-	app.init();
+app.init();
 })();
